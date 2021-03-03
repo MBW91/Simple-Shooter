f8dc27f3e3833836240fc05393089533eff78e1e
Marvin Bernd Walters <mail@marvin-bernd-walters.de>
2021-03-03 17:56:34 +0900

    Lesson 191. Create and Setup an AI controller

---
+++ Source/SimpleShooter/ShooterAIController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAIController.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterAIController : public AAIController
+{
+	GENERATED_BODY()
+};
